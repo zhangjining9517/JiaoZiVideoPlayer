e2eb2046d00da4dd4cdae14a3db9f2695ea89425
zhangjining <168455992@qq.com>
2020-03-31 11:00:11 +0800

    优化滑动频率

--- library/src/main/java/cn/jzvd/Jzvd.java
+++ library/src/main/java/cn/jzvd/Jzvd.java
@@ -277,7 +277,7 @@ public abstract class Jzvd extends FrameLayout implements View.OnClickListener, 
                     }
                     if (mChangePosition) {
                         long totalTimeDuration = getDuration();
-                        mSeekTimePosition = (int) (mGestureDownPosition + deltaX * totalTimeDuration / (mScreenWidth*3));
+                        mSeekTimePosition = (int) ((mGestureDownPosition + deltaX * totalTimeDuration)*0.2 / (mScreenWidth));
                         if (mSeekTimePosition > totalTimeDuration)
                             mSeekTimePosition = totalTimeDuration;
                         String seekTime = JZUtils.stringForTime(mSeekTimePosition);
